92848f210f295a639796c246d000133edd899513
Riece Naidoo <125825660+riecenaidoo@users.noreply.github.com>
2023-05-15 20:27:34 +0200

    Update Webhook Name (#4)
    The Webhook script can now dynamically update the username of the
    Webhook as it posts messages in the Discord chat.

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from discordwebhook import Discord
 
 def get_input():
     """Get non-empty input from the Command Line."""
-    
+
     user_input = input(" > ")
-    while (not user_input):
+    while not user_input:
         user_input = input(" > ")
     return user_input
 
@@ -13,36 +13,43 @@ def get_input():
 def get_webhook(web_hook_file):
     """Open the file containing the webhook and read the URL,
     which should be on a single line."""
-    
+
     try:
         with open(web_hook_file) as f:
             return f.read()
-    except(FileNotFoundError):
+    except FileNotFoundError:
         print(f"WARNING: The specified webhook file '{web_hook_file}' was not found.")
 
 
 if __name__ == "__main__":
-        
-    web_hook_url = get_webhook("web_hook_url.txt" )
+
+    web_hook_url = get_webhook("config/web_hook_url.txt")
     if not web_hook_url:
         print("""
             A web hook URL is needed to start this program.
             See README.md
             """
-            )
+              )
         exit()
 
     print("Starting Session.")
-    discord = Discord(url=web_hook_url) # Create the Webhook
+    discord = Discord(url=web_hook_url)  # Create the Webhook
 
+    custom_name = None
     receiving_input = True
     while receiving_input:
         message = get_input()
-        
-        if (message == "quit"):
+
+        if message.startswith("name="):
+            custom_name = message.split("name=")[1]
+            if len(custom_name) == 0 and custom_name.isalnum():
+                custom_name = None
+            continue
+
+        if message == "quit":
             receiving_input = False
             continue
 
-        discord.post(content=message)   # Send Message
-        
-    print("Session Over.")    
+        discord.post(username=custom_name, content=message)  # Send Message
+
+    print("Session Over.")
